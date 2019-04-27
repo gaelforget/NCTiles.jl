16155ffa8589392d1998706b859e15c2469c92d1
gaelforget <gforget@mit.edu>
2019-04-27 08:02:14 -0400

    set all input / output directories to `examples/`

--- examples/ex_2.jl
+++ examples/ex_2.jl
@@ -1,11 +1,11 @@
 using NCTiles,NCDatasets
 
 # Point to NetCDF file
-interpdir = "/Users/Lauren/Documents/eaps/DarwinModelOutputSamples/tests/state_3d_set1_THETA_run/"
+examplesdir = "examples/"
 selectfields = ["THETA"]
-fname = interpdir*"THETA.0009.nc"
+fname = examplesdir*"THETA.0009.nc"
 
-README = readlines("README")
+README = readlines(examplesdir*"README")
 
 # Get all the metadata from the file and set up NCvars
 ncvars,ncdims,fileatts = readncfile(fname)
@@ -14,7 +14,7 @@ f_missval = get(fileatts,"missing_value",NaN)
 f_ff = get(fileatts,"itile",1)
 f_ntile = get(fileatts,"ntile",1)
 
-filename = "ex2.nc"
+filename = examplesdir*"ex2.nc"
 
 # Create the NetCDF file and populate with dimension and field info
 ds,fieldvars,dimlist = createfile(filename,ncvars,README,fillval = f_fillval,missval=f_missval,ff=f_ff,ntile=f_ntile)
