44ca13ab67115c37ba93582716928eb20b368ebe
gaelforget <gforget@mit.edu>
2021-08-10 17:48:20 -0400

    streamline and update example 5

--- examples/Example5.jl
+++ examples/Example5.jl
@@ -15,16 +15,10 @@
 
 # # Example 5
 #
-# A `ClimGrid` struct from the `ClimateTools.jl` package, including metadata read from file, is written to a netcdf file via `NCTiles.jl`. 
-#
-# _Note: `ClimateTools.jl` requires that requires specific dimension names as follows_
-#
-# - latitude: lat, latitude, rlat, y, yc
-# - longitude: lon, longitude, rlon, x, xc
-# - time: time
+# A `ClimArray` (struct from `ClimateBase.jl`), which includes metadata read from nectdf file, is written back to a netcdf file via `NCTiles.jl`. Then the reverse is done to illustrate consistent workflows.
 
 # +
-using ClimateBase, NCDatasets, NCTiles
+using ClimateBase, NCDatasets, NCTiles, Dates
 
 # File Paths
 inputs=NCTiles.NCTILES_TESTCASES
@@ -35,16 +29,60 @@ if ~ispath(outputs); mkpath(outputs); end
 
 savedir = joinpath(outputs,"ex5")
 if ~ispath(savedir); mkpath(savedir); end
+# -
+
+# ## 1. Read via NCTiles.jl and write via ClimateBase.jl
+
+# ### Helper function
 
 """
-        climgridtoncvar(C::ClimGrid,N::String)
+        NCvar_to_ClimArray(ncvar::NCvar,ncdims::Dict)
 
-Creates an NCvar struct from a ClimArray object. NCvar struct can then be written
-to a NetCDF file using write().
+Creates a ClimArray struct, from a NCvar struct, which can then be written
+to a NetCDF file using `ClimateBase.ncwrite()`.
+"""
+function NCvar_to_ClimArray(ncvar::NCvar,ncdims::Dict)
+    lons=ncdims["lon_c"].values[:]
+    lats=ncdims["lat_c"].values[:]
+    t=ncdims["tim"].values[:]
+    dimensions = (Lon(lons), Lat(lats), ClimateBase.Ti(t))
 
-Ex: C = ClimateBase.ncread(fil, "ETAN")
+    name=ncvar.name
+    data=ncvar.values[:]
+    units=ncvar.units
+    return ClimArray(data, dimensions; name = name, attrib = Dict("units" => units))
+end
+
+# ### Main call sequence : 
+
+# +
+
+fil=joinpath(outputs,"ex1/ex1_NetCDF.nc")
+ncvars,ncdims,fileatts = NCTiles.readncfile(fil)
+
+#filout=joinpath(savedir,"ex5_NCTiles.nc")
+#write(ncvars["ETAN"],filout,globalattribs=fileatts)
+
+filout=joinpath(savedir,"ex5_ClimateBase.nc")
+A=NCvar_to_ClimArray(ncvars["ETAN"],ncdims)
+ClimateBase.ncwrite(filout, A)
+# -
+
+# ## 2. Read via ClimateBase.jl and write via NCTiles.jl
+#
+# ### Helper function
+
+"""
+        ClimArray_to_NCvar(C::ClimGrid,N::String)
+
+Creates an NCvar struct, from a ClimArray struct, which can then be written
+to a NetCDF file using `NCTiles.write()`.
+
+```
+C = ClimateBase.ncread(fil, "ETAN")
 writefld = ClimArray_to_NCvar(C,"ETAN")
-write(writefld,"myfile.nc")
+NCTiles.write(writefld,"myfile.nc")
+```
 """
 function ClimArray_to_NCvar(C::ClimArray,N::String)
         x = C.dims[1][:]
@@ -61,29 +99,17 @@ function ClimArray_to_NCvar(C::ClimArray,N::String)
         return NCvar(N,C.attrib["units"],dims,C.data,C.attrib,NCDatasets)
 end   
 
-## Read via NCTiles.jl and write via ClimateBase.jl
-
-fil=joinpath(outputs,"ex1/ex1_NetCDF.nc")
-ncvars,ncdims,fileatts = readncfile(fil)
-
-#filout=joinpath(savedir,"ex5_NCTiles.nc")
-#write(ncvars["ETAN"],filout,globalattribs=fileatts)
-
-lons=ncdims["lon_c"].values[:];
-lats=ncdims["lat_c"].values[:];
-t=ncdims["tim"].values[:];
-dimensions = (Lon(lons), Lat(lats), ClimateBase.Ti(t))
-
-data=ncvars["ETAN"].values[:];
-units=ncvars["ETAN"].units;
-A = ClimArray(data, dimensions; name = "ETAN", attrib = Dict("units" => units))
-
-filout=joinpath(savedir,"ex5_ClimateBase.nc")
-ncwrite(filout, A)
-
-## Read via ClimateBase.jl and write via NCTiles.jl
+# ### Main call sequence : 
+#
+# _Note: `ClimateBase.jl` relies on specific dimension names to identify space and time dimensions (as follows). This sometimes lead to warnings when files use different names._
+#
+# - latitude: lat, latitude, rlat, y, yc
+# - longitude: lon, longitude, rlon, x, xc
+# - time: time
 
 ETAN = ClimateBase.ncread(fil, "ETAN")
 ETAN = ClimArray_to_NCvar(ETAN,"ETAN")
 filout=joinpath(savedir,"ex5_ClimArray_to_NCvar.nc")
-write(ETAN,filout)
+NCTiles.write(ETAN,filout)
+
+
