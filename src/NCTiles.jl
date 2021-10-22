032146e009ed25f53a99ab67c4db85fda8d58522
gaelforget <gforget@mit.edu>
2021-10-22 09:21:36 -0400

    add NCT struct and write_2D_clim high level API proto

--- src/NCTiles.jl
+++ src/NCTiles.jl
@@ -32,7 +32,7 @@ include("read.jl")
 include("tile_support.jl")
 include("helper_functions.jl")
 
-export NCvar, BinData, NCData, TileData, readbin, readncfile
+export NCT, NCvar, BinData, NCData, TileData, readbin, readncfile
 export createfile, addDim, addVar, addData, addDimData
 export write
 
